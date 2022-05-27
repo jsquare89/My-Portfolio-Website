2717976b0e89f54508cdd8704805df52e49b18c2
Jarred J <jsquare89@users.noreply.github.com>
2022-05-27 16:24:39 -0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== PORTFOLIO MODAL ====================*/
+const modalViews = document.querySelectorAll('.portfolio__modal'),
+      modalBtns = document.querySelectorAll('.portfolio__button'),
+      modalCloses = document.querySelectorAll('.portfolio__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach(modalClose => {
+    modalClose.addEventListener('click', ()=>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
